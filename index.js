eda54868fb14fd2045f4a33d72ff8ef57d5c6be3
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-04-17 09:37:59 +0300

    I updated the necessary code on the file.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+request('datejs');
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  }
+}
 
+args.forEach(arg => {
+  combinedObject.users.push(...arr);
+})
 
+combinedObject.merge_date = new Date().toString('M/d/yyyy');
+return combinedObject;
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
