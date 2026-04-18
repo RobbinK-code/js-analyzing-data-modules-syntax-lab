1885827288f719fa861380645322963365b51cc7
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-04-18 11:58:38 +0300

    Debugging.

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ function combineUsers(...args) {
     users: []
   };
 
-  // Merge all arrays
-  args.forEach(array => {
+ args.forEach(array => {
+  if (Array.isArray(array)) {
     combinedObject.users.push(...array);
-  });
+  }
+});
 
-  // Create today's date properly
   const today = new Date();
 
-  // Use datejs formatting
   combinedObject.merge_date = today.toString('M/d/yyyy');
 
   return combinedObject;
